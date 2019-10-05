f1a58aee92449023f6ba1c6516202d3ee6686393
Ryan Toff <ryantoff@gmail.com>
2019-10-05 18:33:03 -0400

    Initial commit

---
+++ mocking_app.py
@@ -0,0 +1,39 @@
+# Title Mocking string converter app
+# Author: Ryan Toff
+# Original Created Date: 10/5/2019
+# Description: App to convert an input string into camelcasing
+# --so that it is output similar to the Spongebob mocking meme
+
+# Prompt the user for a string to be converted
+original_string = input("Please input a string you would like converted for mocking purposes: ")
+
+# Create a list to hold the characters
+char_list = []
+
+# Split the string into a list of separate characters
+for char in original_string:
+    char_list.append(char)
+
+print(char_list)
+
+print(len(char_list))
+
+# Create variable to iterate over while loop and a list for the converted chars
+i = 0
+converted_char_list = []
+
+# Convert character case based on even/odd
+while i <= (len(char_list) - 1):
+    if (i == 0) or (i % 2 == 0):
+        converted_char_list.append(char_list[i].lower())
+    else:
+        converted_char_list.append(char_list[i].upper())
+    i+=1
+
+# Create variable to hold the converted string
+converted_string = ""
+for char in converted_char_list:
+    converted_string += char
+
+print(converted_char_list)
+print(converted_string)
