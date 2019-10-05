648b6009bc4e0acd9f0c1ecc2aaa83e78cef8ad3
Ryan Toff <ryantoff@gmail.com>
2019-10-05 19:02:26 -0400

    Converted to Flask app. Logic in temp.py

--- mocking_app.py
+++ mocking_app.py
@@ -4,36 +4,18 @@
 # Description: App to convert an input string into camelcasing
 # --so that it is output similar to the Spongebob mocking meme
 
-# Prompt the user for a string to be converted
-original_string = input("Please input a string you would like converted for mocking purposes: ")
 
-# Create a list to hold the characters
-char_list = []
+########### CONVERT temp.py TO FLASK APP ###########
+# Import flask modules
+from flask import Flask, render_template, redirect
 
-# Split the string into a list of separate characters
-for char in original_string:
-    char_list.append(char)
+# Create Flask APP
+app = Flask(__name__)
 
-print(char_list)
+# Create main page
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-print(len(char_list))
-
-# Create variable to iterate over while loop and a list for the converted chars
-i = 0
-converted_char_list = []
-
-# Convert character case based on even/odd
-while i <= (len(char_list) - 1):
-    if (i == 0) or (i % 2 == 0):
-        converted_char_list.append(char_list[i].lower())
-    else:
-        converted_char_list.append(char_list[i].upper())
-    i+=1
-
-# Create variable to hold the converted string
-converted_string = ""
-for char in converted_char_list:
-    converted_string += char
-
-print(converted_char_list)
-print(converted_string)
+if __name__ == '__main__':
+    app.run(debug=True)
