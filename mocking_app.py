226577b5bd6df05d7906a39cb051bb8f88c5ffa7
Ryan Toff <ryantoff@gmail.com>
2019-10-06 17:42:46 -0400

    Converted logic to Flask App with templates

--- mocking_app.py
+++ mocking_app.py
@@ -7,7 +7,7 @@
 
 ########### CONVERT temp.py TO FLASK APP ###########
 # Import flask modules
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, redirect
 
 # Create Flask APP
 app = Flask(__name__)
@@ -16,6 +16,36 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/converted_phrase')
+def converted_phrase():
+    # Take in the string submitted from index.html
+    original_string = request.args.get('original_string')
+
+    # Create a list to hold the characters
+    char_list = []
+
+    # Split the string into a list of separate characters
+    for char in original_string:
+        char_list.append(char)
+
+    # Create variable to iterate over while loop and a list for the converted chars
+    i = 0
+    converted_char_list = []
+
+    # Convert character case based on even/odd
+    while i <= (len(char_list) - 1):
+        if (i == 0) or (i % 2 == 0):
+            converted_char_list.append(char_list[i].lower())
+        else:
+            converted_char_list.append(char_list[i].upper())
+        i+=1
+
+    # Create variable to hold the converted string
+    converted_string = ""
+    for char in converted_char_list:
+        converted_string += char
+    return render_template('converted_phrase.html',original_string=original_string,converted_string=converted_string)
 
 if __name__ == '__main__':
     app.run(debug=True)
