d03df84d69dc76947ac325c54a72bcd0d5777396
Ryan Toff <ryantoff@gmail.com>
2019-10-10 15:00:38 -0400

    Convert to use FlaskForms

--- mocking_app.py
+++ mocking_app.py
@@ -7,45 +7,60 @@
 
 ########### CONVERT temp.py TO FLASK APP ###########
 # Import flask modules
-from flask import Flask, render_template, redirect, request, redirect
+from flask import Flask, render_template, redirect, request, flash, session, url_for
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create Flask APP
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+class SassForm(FlaskForm):
+    original_string = TextAreaField("Enter a phrase, I'll sass you all day.", validators=[DataRequired()])
+    submit = SubmitField('Sass Me')
+
 # Create main page
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
 
-@app.route('/converted_phrase')
-def converted_phrase():
-    # Take in the string submitted from index.html
-    original_string = request.args.get('original_string')
+    form = SassForm()
 
-    # Create a list to hold the characters
-    char_list = []
+    if form.validate_on_submit():
+        # Take in the string submitted from index.html
+        session['original_string'] = form.original_string.data
 
-    # Split the string into a list of separate characters
-    for char in original_string:
-        char_list.append(char)
+        # Create a list to hold the characters
+        char_list = []
 
-    # Create variable to iterate over while loop and a list for the converted chars
-    i = 0
-    converted_char_list = []
+        # Split the string into a list of separate characters
+        for char in session['original_string']:
+            char_list.append(char)
 
-    # Convert character case based on even/odd
-    while i <= (len(char_list) - 1):
-        if (i == 0) or (i % 2 == 0):
-            converted_char_list.append(char_list[i].lower())
-        else:
-            converted_char_list.append(char_list[i].upper())
-        i+=1
+        # Create variable to iterate over while loop and a list for the converted chars
+        i = 0
+        converted_char_list = []
 
-    # Create variable to hold the converted string
-    converted_string = ""
-    for char in converted_char_list:
-        converted_string += char
-    return render_template('converted_phrase.html',original_string=original_string,converted_string=converted_string)
+        # Convert character case based on even/odd
+        while i <= (len(char_list) - 1):
+            if (i == 0) or (i % 2 == 0):
+                converted_char_list.append(char_list[i].lower())
+            else:
+                converted_char_list.append(char_list[i].upper())
+            i+=1
+
+        # Create variable to hold the converted string
+        converted_string = ""
+        for char in converted_char_list:
+            converted_string += char
+
+        # Flash the converted string to the user
+        flash(converted_string)
+
+        return redirect(url_for('index'))
+
+    return render_template('index.html',form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
